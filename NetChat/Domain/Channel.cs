e4f8fbeaa29546a830685bb3857a47682fa5e4e0
kenlly <kenllynovas012@gmail.com>
2026-02-16 20:56:11 -0400

    Upload the project

---
+++ NetChat/Domain/Channel.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain
+{
+    public class Channel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+    }
+}
---
+++ NetChat/Persistence/AplicationDataContext.cs
@@ -0,0 +1,15 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class AplicationDataContext:DbContext
+    {
+        public AplicationDataContext(DbContextOptions options): base(options)
+        {
+                
+        }
+        public DbSet<Channel> Channels { get; set; }
+
+    }
+}
